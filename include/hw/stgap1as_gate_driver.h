805629069e4730277b662213a393bdfc0d5db2e7
David J. Fiddes <D.J@fiddes.net>
2021-03-19 13:42:54 +0000

    Add support for the Tesla Model 3 gate drivers
    This change adds a driver supporting the configuration,
    operation and monitoring of the isolated gate drivers on the
    Tesla Model 3 inverter. The gate drivers are initialised
    following the same pattern as the logic analyser captures
    from https://github.com/damienmaguire/Tesla-Model-3-Drive-Unit/blob/master/GATE_SPI/RDU_capture1_PhaseB_L_Startup.logicdata
    at commit  ea0cb15.
    
    The driver:
     - initialises and the verifies the configuration of the 6 daisy
       chained STGAP1AS driver chips
     - Allows the gate drivers to be enabled and disabled when
       required using an associated GPIO output
     - Allows the state of the gate drivers to be monitored for a
       fault condition
    
    stm32-sine integration is limited to the basic initialisation
    and enabling of gate drive when required and raising two
    new faults when they occur.
    
    The integration does not (but possibly should in the future):
     - reset and retry initialisation on failure
     - store the fault details in a status parameter
    
    Hardware mapping of SPI controller and GPIO pins is
    speculative and will need updating according to the HW
    design.
    
    Tests:
     - The TeslaM3GateDriver class has been tested extensively
       in an out-of-tree libopencm3 application running on an
       STM32 Bluepill board.
     - Verification of SPI initialisation sequence with logic
       analyser checking against the Tesla FW captures. These
       match apart from the expected differences when re-
       querying the config (this code uses 6 register reads - telsa
       uses interleaved read/nop for high/low-side driver
       settings)
     - Initialisation and verification were run against a crude
       ESP32 based STGAP1AS simulator was mostly successful.
       Problems remain isolated to shortcomings in the ESP32
       SPI slave.
     - Request timing was compared via logic analyser against
       the datasheet for correctness. Inter-request timing is
       tighter than Tesla FW mostly.
     - Modifying the stm32-sine integration to run on a Bluepill
       allowed verification of the generation of the two new
       faults for initialisation and fault status checking.

--- include/hw/stgap1as_gate_driver.h
+++ include/hw/stgap1as_gate_driver.h
@@ -29,6 +29,17 @@
  *
  */
 
+/** @defgroup spi_crc SPI Communication CRC Constants
+@{*/
+
+/** SPI communication crc polynomial corresponding to x^8 + x^2 + x + 1 */
+#define STGAP1AS_SPI_CRC_POLYNOMIAL             0x07
+
+/** SPI communication CRC initial value */
+#define STGAP1AS_SPI_CRC_INIT_VALUE             0xFF
+
+/**@}*/
+
 /** @defgroup spi_commands SPI Commands
 @{*/
 
@@ -41,13 +52,16 @@
 /** No operation */
 #define STGAP1AS_CMD_NOP                        (0b00000000)
 
+#define STGAP1AS_CMD_REG_MASK			        (0b11100000)
+#define STGAP1AS_CMD_WRITE_REG_VALUE			(0b10000000)
 #define STGAP1AS_CMD_WRITE_REG_MASK			    0x1F
 /** Write register */
-#define STGAP1AS_CMD_WRITE_REG(x)               (0b10000000 | (x & STGAP1AS_CMD_WRITE_REG_MASK))
+#define STGAP1AS_CMD_WRITE_REG(x)               (STGAP1AS_CMD_WRITE_REG_VALUE | (x & STGAP1AS_CMD_WRITE_REG_MASK))
 
+#define STGAP1AS_CMD_READ_REG_VALUE			    (0b10100000)
 #define STGAP1AS_CMD_READ_REG_MASK			    0x1F
 /** Write register */
-#define STGAP1AS_CMD_READ_REG(x)                (0b10100000 | (x & STGAP1AS_CMD_READ_REG_MASK))
+#define STGAP1AS_CMD_READ_REG(x)                (STGAP1AS_CMD_READ_REG_VALUE | (x & STGAP1AS_CMD_READ_REG_MASK))
 
 /** Reset all the status registers */
 #define STGAP1AS_CMD_RESET_STATUS               (0b11010000)
@@ -305,7 +319,7 @@
 /** VDD overvoltage flag */
 #define STGAP1AS_REG_STATUS3_OVLOD              (1 << 1)
 
-/** DD undervoltage flag */
+/** VDD undervoltage flag */
 #define STGAP1AS_REG_STATUS3_UVLOD              (1 << 0)
 
 /**@}*/
